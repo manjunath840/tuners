c6e833415cec882d9d861891eac5cc2b7fc2821d
MANJUNATH M R <MANJUNATH M R@DESKTOP-FVVPMEF>
2019-07-31 13:16:47 +0530

    this is my repossitorry

---
+++ src/test/java/test/DemoQ.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoQ 
+{
+	@Test
+	public void createUser()
+	{
+		Reporter.log("manju", true);
+	}
+	
+	
+
+}
